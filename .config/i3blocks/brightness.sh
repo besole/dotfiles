864ebf592b164f0146dd7f126152f58c19fa549f
Benjamin Solenthaler <mail@besole.ch>
2019-11-15 13:53:14 +0100

    simplified brightness controll

--- .config/i3blocks/brightness.sh
+++ .config/i3blocks/brightness.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-function currentBrightnessPercentage {
-  CURRENT=$( brightnessctl get )
-  MAX=$(brightnessctl max )
-  PERCENTAGE=$((100 * CURRENT / MAX))
-  echo ${PERCENTAGE}
-}
-function getDevice {
-  CURRENT=$( currentBrightnessPercentage )
-  echo " ${CURRENT}%"
-}
-function incBrightness {
-  CURRENT=$( currentBrightnessPercentage )
-  NEW=$(( CURRENT + 5 ))
-  sudo brightnessctl set ${NEW}%
-}
-function decBrightness {
-  CURRENT=$( currentBrightnessPercentage )
-  NEW=$(( CURRENT - 5 ))
-  sudo brightnessctl set ${NEW}%
-}
-
-[[ "$1" == "+" ]] && incBrightness
-[[ "$1" == "-" ]] && decBrightness
-getDevice
-
+CURRENT=$( brightnessctl get )
+MAX=$(brightnessctl max )
+PERCENTAGE=$((100 * CURRENT / MAX))
+echo " ${PERCENTAGE}%"
