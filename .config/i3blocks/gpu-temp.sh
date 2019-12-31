d730e4687bba0b2698ee258ffe6c68f15cfa18f0
Benjamin Solenthaler <mail@besole.ch>
2019-12-31 15:01:19 +0100

    Adding GPU temperature script - work still in progress

---
+++ .config/i3blocks/gpu-temp.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Nouveau
+#TEMP=$(cat /sys/bus/pci/devices/0000:01:00.0/hwmon/*/temp1_input)
+#TEMP=$((TEMP / 1000))
+
+# Nvidia
+TEMP=$(nvidia-settings -t -q '[gpu:0]/gpucoretemp')
+
+# Output
+echo " ${TEMP}°C"
