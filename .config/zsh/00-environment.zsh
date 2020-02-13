fb7d37de367c0fef9664308aca2ed5935e580857
Benjamin Solenthaler <mail@besole.ch>
2020-02-13 19:41:56 +0100

    updating zsh environment

--- .config/zsh/00-environment.zsh
+++ .config/zsh/00-environment.zsh
@@ -1,9 +1,11 @@
 # Environment
-export EDITOR="vim"
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
 export EDITOR="vim"
 export PAGER="less"
 export LESS="-FRX"
-export LESSHISTFILE=/dev/null
 
 export TERM=xterm-256color
