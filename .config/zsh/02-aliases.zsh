282fb3dca9a9f37df1a771162e990a308c304898
Benjamin Solenthaler <mail@besole.ch>
2020-06-12 22:33:19 +0200

    redirect xinit log to file in tmp

--- .config/zsh/02-aliases.zsh
+++ .config/zsh/02-aliases.zsh
@@ -1,4 +1,4 @@
 # Aliases
 alias feh="feh --scale-down"
-alias xinit="xinit -- :${XDG_VTNR}"
+alias xinit="xinit -- :${XDG_VTNR} &> /tmp/xinit-${USER}-${XDG_VTNR}"
 alias picom="picom --experimental-backends"
