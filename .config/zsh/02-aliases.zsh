706a91d276a34b2e559a833c6faff8e4bb1acc33
Benjamin Solenthaler <mail@besole.ch>
2020-07-10 20:28:29 +0200

    bitwarden search alias

--- .config/zsh/02-aliases.zsh
+++ .config/zsh/02-aliases.zsh
@@ -2,3 +2,4 @@
 alias feh="feh --scale-down"
 alias xinit="xinit -- :${XDG_VTNR} &> /tmp/xinit-${USER}-${XDG_VTNR}"
 alias picom="picom --experimental-backends"
+alias bw-search="bw --pretty list items --search"
