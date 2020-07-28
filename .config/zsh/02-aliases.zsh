03274f00c21b0df3423090ce62a89de7663e725b
Benjamin Solenthaler <mail@besole.ch>
2020-07-28 21:21:17 +0200

    moving from xinit to startx, changing xserver arguments

--- .config/zsh/02-aliases.zsh
+++ .config/zsh/02-aliases.zsh
@@ -1,5 +1,6 @@
 # Aliases
 alias feh="feh --scale-down"
-alias xinit="xinit -- :${XDG_VTNR} &> /tmp/xinit-${USER}-${XDG_VTNR}"
+alias xinit="echo 'Please use startx!'"
+alias startx="startx '${XDG_CONFIG_HOME}/X11/xinitrc' -- '${XDG_CONFIG_HOME}/X11/xserverrc' &> /tmp/xinit-${USER}-${XDG_VTNR}"
 alias picom="picom --experimental-backends"
 alias bw-search="bw --pretty list items --search"
