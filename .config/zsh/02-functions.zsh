3b8d744563b348acc712f7fed295446a3383df6a
Benjamin Solenthaler <mail@besole.ch>
2020-07-28 21:30:29 +0200

    enhancing bw-search

--- .config/zsh/02-functions.zsh
+++ .config/zsh/02-functions.zsh
@@ -5,3 +5,8 @@ randslide(){
 		ls -A | shuf | sxiv -a -b -p -S10 -sf -
 	)
 }
+
+# Search Bitwarden and grep for needed data
+bw-search(){
+	bw --pretty list items --search "$@" | grep -E '"name"|"username"|"password"'
+}
