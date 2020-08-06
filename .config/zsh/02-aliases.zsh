aa88bc1cd15b6ce8ddbedc7781c92e36f5c3215d
Benjamin Solenthaler <mail@besole.ch>
2020-08-06 09:26:47 +0200

    always starting X on current VT

--- .config/zsh/02-aliases.zsh
+++ .config/zsh/02-aliases.zsh
@@ -1,5 +1,5 @@
 # Aliases
 alias feh="feh --scale-down"
 alias xinit="echo 'Please use startx!'"
-alias startx="startx '${XDG_CONFIG_HOME}/X11/xinitrc' -- '${XDG_CONFIG_HOME}/X11/xserverrc' &> /tmp/xinit-${USER}-${XDG_VTNR}"
+alias startx="startx '${XDG_CONFIG_HOME}/X11/xinitrc' -- '${XDG_CONFIG_HOME}/X11/xserverrc' ':${XDG_VTNR}' 'vt${XDG_VTNR}' &> /tmp/xinit-${USER}-${XDG_VTNR}"
 alias picom="picom --experimental-backends"
