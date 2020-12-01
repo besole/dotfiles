6c16a9cc9184a5f1c90ab0604d0be9719dcf6314
Benjamin Solenthaler <mail@besole.ch>
2020-12-01 17:35:13 +0100

    automatically check for nvidia driver

--- .config/i3blocks/gpu-temp.sh
+++ .config/i3blocks/gpu-temp.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
-# Nouveau
-#TEMP=$(cat /sys/bus/pci/devices/0000:01:00.0/hwmon/*/temp1_input)
-#TEMP=$((TEMP / 1000))
-
-# Nvidia
-TEMP=$(nvidia-settings -t -q '[gpu:0]/gpucoretemp')
+if lsmod | grep -i 'nvidia' &>/dev/null; then
+	# Nvidia
+	TEMP=$(nvidia-settings -t -q '[gpu:0]/gpucoretemp')
+else
+	# Nouveau
+	TEMP=$(cat /sys/bus/pci/devices/0000:01:00.0/hwmon/*/temp1_input)
+	TEMP=$((TEMP / 1000))
+fi
 
 # Output
 echo " ${TEMP}°C"
