2da35c9fa74515fc6ccccb7e98605310276ee570
Benjamin Solenthaler <mail@besole.ch>
2021-11-06 16:55:51 +0100

    updating aliases

--- .config/zsh/20-aliases.zsh
+++ .config/zsh/20-aliases.zsh
@@ -1,10 +1,11 @@
 # Colors
-alias ls="ls --color"
-alias diff="diff --color"
-alias grep="grep --color"
+alias ls="ls --color=auto"
+alias diff="diff --color=auto"
+alias grep="grep --color=auto"
 
 # Aliases
 alias feh="/usr/bin/feh --scale-down"
 alias xinit="echo 'Please use startx!'"
-alias startx="/usr/bin/startx '${XDG_CONFIG_HOME}/X11/xinitrc' -- '${XDG_CONFIG_HOME}/X11/xserverrc' ':${XDG_VTNR}' 'vt${XDG_VTNR}' &> /tmp/xinit-${USER}-${XDG_VTNR}"
+alias startx="/usr/bin/startx '${XDG_CONFIG_HOME}/X11/xinitrc' -- '${XDG_CONFIG_HOME}/X11/xserverrc' 2>&1 | tee /tmp/xinit-${USER}-${XDG_VTNR}"
 alias sway="sway-run.sh &> '${HOME}/.local/share/sway/log-${USER}-${XDG_VTNR}'"
+alias scrcpy="/usr/bin/scrcpy --disable-screensaver --turn-screen-off --stay-awake"
