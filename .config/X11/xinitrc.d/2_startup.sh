b4be8031315363aa40ce6fd6920576a06ff6673a
Benjamin Solenthaler <benjamin.solenthaler@students.ffhs.ch>
2021-11-06 14:42:19 +0100

    starting vdagent, removing unused apps, adding monitor modes

--- .config/X11/xinitrc.d/2_startup.sh
+++ .config/X11/xinitrc.d/2_startup.sh
@@ -1,8 +1,8 @@
 eval $( /usr/bin/gnome-keyring-daemon --start )
 export SSH_AUTH_SOCK
 
+spice-vdagent
 numlockx &
 unclutter --timeout 2 --jitter 10 --ignore-scrolling &
 feh --no-fehbg --bg-fill "${HOME}/.config/feh/current" &
-nextcloud &
 dunst &
