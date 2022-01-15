fe78a357e84daa5cba9ebf7c645cd71032feb8c7
Benjamin Solenthaler <mail@besole.ch>
2022-01-15 15:52:57 +0100

    moving X11 arguments to alias

--- .config/zsh/20-aliases.zsh
+++ .config/zsh/20-aliases.zsh
@@ -6,6 +6,6 @@ alias grep="grep --color=auto"
 # Aliases
 alias feh="/usr/bin/feh --scale-down"
 alias xinit="echo 'Please use startx!'"
-alias startx="/usr/bin/startx '${XDG_CONFIG_HOME}/X11/xinitrc' -- '${XDG_CONFIG_HOME}/X11/xserverrc' 2>&1 | tee /tmp/xinit-${USER}-${XDG_VTNR}"
+alias startx="/usr/bin/startx '${XDG_CONFIG_HOME}/X11/xinitrc' -- '${XDG_CONFIG_HOME}/X11/xserverrc' ':${XDG_VTNR}' 'vt${XDG_VTNR}' 2>&1 | tee /tmp/xinit-${USER}-${XDG_VTNR}"
 alias sway="sway-run.sh &> '${HOME}/.local/share/sway/log-${USER}-${XDG_VTNR}'"
 alias scrcpy="/usr/bin/scrcpy --disable-screensaver --turn-screen-off --stay-awake"
