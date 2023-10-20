d6d392b7ad7e3fc33def6fe0d150115801ef4ed8
Benjamin Solenthaler <mail@besole.ch>
2023-10-20 17:41:35 +0200

    remove(scripts): removing obsolete monitor scripts

--- .local/bin/single-monitor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -n "$DISPLAY" ]; then
-	xrandr \
-		--output Virtual-1 --primary --mode 1920x1080 \
-		--output Virtual-2 --off \
-		--output Virtual-3 --off
-fi
--- .local/bin/triple-monitor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -n "$DISPLAY" ]; then
-	xrandr \
-		--output Virtual-1 --mode 1920x1080 --left-of Virtual-2 \
-		--output Virtual-2 --mode 1920x1080 --primary \
-		--output Virtual-3 --mode 1920x1080 --right-of Virtual-2
-fi
