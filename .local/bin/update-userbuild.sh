5967a08380fa7ec958aefc76c499428c0bfb024c
Benjamin Solenthaler <mail@besole.ch>
2023-10-20 12:06:41 +0200

    change(scripts): check userbuild updates in parallel

--- .local/bin/update-userbuild.sh
+++ .local/bin/update-userbuild.sh
@@ -5,13 +5,16 @@ REPOSITORIES=$( find -L "${USERBUILD}" -type d -name ".git" -exec dirname {} \; 
 
 RESULT=0
 while read -r repo; do
-	git -C "${repo}" fetch origin &> /dev/null
-	REMOTECOMMIT=$( git -C "${repo}" rev-parse origin/master )
-	LOCALCOMMIT=$( git -C "${repo}" rev-parse master )
-	if [[ "${REMOTECOMMIT}" != "${LOCALCOMMIT}" ]]; then
-		RESULT=1
-		echo "Please update ${repo}"
-	fi
+	(
+		git -C "${repo}" fetch origin &> /dev/null
+		REMOTECOMMIT=$( git -C "${repo}" rev-parse origin/master )
+		LOCALCOMMIT=$( git -C "${repo}" rev-parse master )
+		if [[ "${REMOTECOMMIT}" != "${LOCALCOMMIT}" ]]; then
+			RESULT=1
+			echo "Please update ${repo}"
+		fi
+	) &
 done <<< "${REPOSITORIES}"
 
+wait $(jobs -p)
 exit "${RESULT}"
