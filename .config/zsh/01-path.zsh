0ce6dc5dd28bf5dc9925501c92edca645c4322d6
Benjamin Solenthaler <bsolenthaler@sasag.ch>
2025-12-03 16:00:22 +0100

    fix(zsh): check full path in path append/prepend matching

--- .config/zsh/01-path.zsh
+++ .config/zsh/01-path.zsh
@@ -2,14 +2,14 @@
 
 append_path () {
 	case ":$PATH:" in
-		*:"$1"*) ;;
+		*:"$1":*) ;;
 		*) PATH="${PATH:+$PATH:}$1" ;;
 	esac
 }
 
 prepend_path () {
 	case ":$PATH:" in
-		*:"$1"*) ;;
+		*:"$1":*) ;;
 		*) PATH="$1${PATH:+:$PATH}";;
 	esac
 }
